93a15d4c53725fd581f7937a368dc4f1b095cdd6
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-08-11 23:25:26 -0300

    Create Box.java

---
+++ TAD/Hash/Box.java
@@ -0,0 +1,19 @@
+package Hash;
+
+public class Box {
+    public int key;
+    public int element;
+
+    public Box(int key, int element) {
+        this.key = key;
+        this.element = element;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getElement() {
+        return element;
+    }
+}
